d572ee22511403aab178248d6a00107f2a8aa039
zhang <1774761877@qq.com>
2022-03-02 23:26:35 +0800

    修改侧边栏
    修改侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,56 +1,79 @@
 module.exports = {
-  title: '个人主页',
-  description: 'Personal Website',
-  head: [ // 注入到当前页面的 HTML <head> 中的标签
-    ['link', { rel: 'icon', href: '/images/photo.jpg' }],
-    ['link', { rel: 'manifest', href: '/images/photo.jpg' }],
-    ['link', { rel: 'apple-touch-icon', href: '/images/photo.jpg' }],
-    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache'}],
-    ['meta', { 'http-quiv': 'pragma', cotent: 'no-cache,must-revalidate'}],
-    ['meta', { 'http-quiv': 'expires', cotent: '0'}]
+  title: "张晓伟-技术文档",
+  description: "Personal Website",
+  head: [
+    // 注入到当前页面的 HTML <head> 中的标签
+    ["link", { rel: "icon", href: "/images/photo.jpg" }],
+    ["link", { rel: "manifest", href: "/images/photo.jpg" }],
+    ["link", { rel: "apple-touch-icon", href: "/images/photo.jpg" }],
+    ["meta", { "http-quiv": "pragma", cotent: "no-cache" }],
+    ["meta", { "http-quiv": "pragma", cotent: "no-cache,must-revalidate" }],
+    ["meta", { "http-quiv": "expires", cotent: "0" }],
   ],
   serviceWorker: true, // 是否开启 PWA
-  base: './', // 部署到github相关的配置
+  base: "/", // 部署到github相关的配置
   markdown: {
-    lineNumbers: true // 代码块是否显示行号
+    lineNumbers: true, // 代码块是否显示行号
   },
   themeConfig: {
-    nav:[ // 导航栏配置
-      {text: '前端基础', link: '/accumulate/' },
-      {text: '算法题库', link: '/algorithm/'},
-      {text: '诗和远方', link: '/others/'},
-      {text: '微博', link: 'https://baidu.com'}      
+    nav: [
+      // 导航栏配置
+      { text: "前端基础", link: "/accumulate/" },
+      { text: "大前端", link: "/algorithm/" },
+      { text: "诗和远方", link: "/life/" },
+      { text: "个人组件库", link: "https://helloonetwo.github.io/xiao-ui-website" },
     ],
-    // sidebar:{
-    //   '/accumulate/': [
-    //       {
-    //         title: '前端积累',
-    //         children: [
-    //           '/accumulate/1.html',
-    //           '/accumulate/2.html',
-    //           '/accumulate/3.html',
-    //           '/accumulate/4.html',
-    //           '/accumulate/5.html',
-    //           '/accumulate/6.html',
-    //           '/accumulate/7.html',
-    //           '/accumulate/8.html',
-    //           '/accumulate/9.html',
-    //           '/accumulate/10.html',
-    //           '/accumulate/11.html',
-    //         ]
-    //       }
-    //     ],
-    //     '/algorithm/': [
-    //       '/algorithm/', 
-    //       {
-    //         title: '第二组侧边栏下拉框的标题1',
-    //         children: [
-    //           '/algorithm/' 
-    //         ]
-    //       }
-    //     ]
-    // },
-    sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2
-  }
+    sidebar: {
+      '/accumulate/': [
+        {
+          title: '前端积累',
+          children: [
+            '/accumulate/1.html',
+            '/accumulate/2.html',
+            '/accumulate/3.html',
+            '/accumulate/4.html',
+            '/accumulate/5.html',
+            '/accumulate/6.html',
+            '/accumulate/7.html',
+            '/accumulate/8.html',
+            '/accumulate/9.html',
+            '/accumulate/10.html',
+            '/accumulate/11.html',
+          ]
+        }
+      ],
+      '/algorithm/': [
+        // '/algorithm/',
+        {
+          title: 'JavaScript 深度剖析',
+          children: [
+            "",
+            "手写 Promie",
+            "JS 异步编程"
+          ]
+        },
+        {
+          title: '前端工程化实战',
+          children: [
+            '/algorithm/',
+
+          ]
+        },
+        {
+          title: 'Vue.js 框架源码与进阶',
+          children: [
+            '/algorithm/'
+          ]
+        }
+      ],
+      "/life/": [
+        "",
+        "魔都折叠",
+        "莎翁的智慧"
+      ]
+
+    },
+    // sidebar: "auto", // 侧边栏配置
+    sidebarDepth: 1,
+  },
 };
